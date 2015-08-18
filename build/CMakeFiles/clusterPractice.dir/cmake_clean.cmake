7252759506474a1192cab431fd004915fae1f1d3
albertJ32 <00b0d14c@opayq.com>
2015-08-18 16:59:05 +0100

    initial commit

---
+++ build/CMakeFiles/clusterPractice.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/clusterPractice.dir/clusterPractice.cpp.o"
+  "clusterPractice.pdb"
+  "clusterPractice"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/clusterPractice.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ clusterPractice.cpp
@@ -0,0 +1,24 @@
+// Practice for Clustering with Opencv BOW classes
+
+#include "opencv2/highgui/highgui.hpp" // Needed for HistCalc
+#include "opencv2/imgproc/imgproc.hpp" // Needed for HistCalc
+#include <opencv2/opencv.hpp>
+#include "opencv2/core/core.hpp"
+#include <iostream> // General io
+#include <stdlib.h>
+#include <fstream>
+#include <string>
+#include <algorithm> // Maybe fix DescriptorExtractor doesn't have a member 'create'
+#include <boost/filesystem.hpp>
+
+#include <iostream>
+
+
+using namespace cv;
+using namespace std;
+
+int main(){
+  cout << "inside.." << endl;
+
+  return 0;
+}
